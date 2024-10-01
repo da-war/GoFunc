f885c505427d9be41c910fbfb833dc499b7cf83e
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-10-01 09:38:15 +0500

    Functions as parameters

--- main.go
+++ main.go
@@ -4,8 +4,20 @@ import "fmt"
 
 func main() {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	doubleNumbers(numbers)
+	doubledArray := transformNumbers(numbers, doubleNumber)
+	tripledArray := transformNumbers(numbers, tripleNumber)
+
 	fmt.Println("Original", numbers)
+	fmt.Println("Doubled", doubledArray)
+	fmt.Println("Tripled", tripledArray)
+}
+
+func transformNumbers(numbers []int, fn func(int) int) []int {
+	transformed := []int{}
+	for i := range numbers {
+		transformed = append(transformed, fn(numbers[i]))
+	}
+	return transformed
 }
 
 func doubleNumbers(numbers []int) []int {
@@ -16,4 +28,12 @@ func doubleNumbers(numbers []int) []int {
 	fmt.Println("Doubled", dNumber)
 	return dNumber
 
+}
+
+func doubleNumber(n int) int {
+	return n * 2
+}
+
+func tripleNumber(n int) int {
+	return n * 3
 }
