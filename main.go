092277ba5dfaa0bba3c4861a0a3fa0889421fe19
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-10-01 09:25:01 +0500

    Using funcs

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
-	numbeers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	doubleNumbers(numbers)
+	fmt.Println("Original", numbers)
+}
+
+func doubleNumbers(numbers []int) []int {
+	dNumber := []int{}
+	for i := range numbers {
+		dNumber = append(dNumber, numbers[i]*2)
+	}
+	fmt.Println("Doubled", dNumber)
+	return dNumber
+
 }
